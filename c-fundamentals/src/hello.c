64bbcce64789cc3030330a7c8c7b8eab8a6c44ed
wayson1994 <1097716205@qq.com>
2025-09-30 14:35:54 +0800

    add hello.c

---
+++ c-fundamentals/src/hello.c
@@ -0,0 +1,17 @@
+/**
+ * @file hello.c
+ * @brief 学习仓库入口文件，演示最小 C 程序框架。
+ * @author wayson1994
+ * @date   2025-09-30
+ *
+ * 版权信息：MIT License，详见 LICENSE 文件。
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello C!\n");
+
+    return 0;
+}
