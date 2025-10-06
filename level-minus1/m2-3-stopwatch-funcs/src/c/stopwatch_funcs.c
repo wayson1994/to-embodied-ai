00243e7ed4627905786a20f0f4a7bb99f7406f61
wayson1994 <1097716205@qq.com>
2025-10-06 22:41:52 +0800

    update file

---
+++ level-minus1/m2-3-stopwatch-funcs/src/c/stopwatch_funcs.c
@@ -0,0 +1,23 @@
+/**
+ * @file     stopwatch_funcs.c
+ * @brief    catch copy 1MB memory time
+ * @version  1.0
+ * @author   Wayson
+ * @date     2025.10.6
+ **/
+
+// TODO:catch copy 1MB memory time
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    //long starttime = clock_gettime(CLOCK_MONOTONIC_RAW, );
+
+
+}
