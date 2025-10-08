6008409f7b0a1ec64984f87272b0d97c19629f7a
wayson1994 <1097716205@qq.com>
2025-10-08 22:40:24 +0800

    python study

---
+++ level-0/python/runoob/hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Copyright 2025 Wayson
+# SPDX-License-Identifier: MIT
+# License text: https://opensource.org/licenses/MIT
+
+print("hello world")
---
+++ level-0/python/runoob/val.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Copyright 2025 Wayson
+# SPDX-License-Identifier: MIT
+# License text: https://opensource.org/licenses/MIT
+
+counter = 100 # int
+miles = 1000.0 # float
+name =  "jack" # string
+
+print(counter)
+print(miles)
+print(name)
+
+a = b = 1
+print(a, b)
