c8453e7f6c30417a7ac15c101d70f1c716bf0a28
wayson1994 <1097716205@qq.com>
2025-10-09 21:22:30 +0800

    update val.py

--- level-0/python/runoob/val.py
+++ level-0/python/runoob/val.py
@@ -5,11 +5,22 @@
 
 counter = 100 # int
 miles = 1000.0 # float
-name =  "jack" # string
+name =  "jack" # str
 
 print(counter)
 print(miles)
 print(name)
 
+# multivariate assignment
 a = b = 1
 print(a, b)
+
+a, b, c = 1, 2, "jack"
+print(a, b, c)
+
+is_active = True
+
+print(type(a)) # <class 'int'>
+print(type(b)) # <class 'int'>
+print(type(c)) # <class 'str'>
+print(type(is_active)) # # <class 'bool'>
